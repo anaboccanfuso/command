2a4399407368ad2a9218a913fe719a03056abbbc
anaboccanfuso <77176581+anaboccanfuso@users.noreply.github.com>
2021-10-13 13:06:17 -0400

    Added JavaDoc

--- IDontKnowCommand.java
+++ IDontKnowCommand.java
@@ -1,10 +1,22 @@
+/**
+ * A class to print out the lyrics to I Don't Know by calling
+ * the Military Cadence class.
+ * @author Ana Boccanfuso
+ */
 public class IDontKnowCommand implements Command {
     private MilitaryCadence cadence;
 
+    /**
+     * Creates a specific instance of the cadence. 
+     * @param cadence The song to be sung.
+     */
     public IDontKnowCommand(MilitaryCadence cadence) {
         this.cadence = cadence;
     }
 
+    /**
+     * Calls the Military Cadence class to print the cadence.
+     */
     public void execute() {
         cadence.singIDontKnow();
     }
