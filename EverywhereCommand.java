aca3ad310c3a71288f70b21d897eba37418f2f15
taaare <adamsanfacon@gmail.com>
2021-10-13 13:03:04 -0400

    added javadocs

--- EverywhereCommand.java
+++ EverywhereCommand.java
@@ -1,10 +1,15 @@
+/**
+ * A class to play specific song EverywhereWeGo
+ * @author adam sanfacon
+ */
 public class EverywhereCommand implements Command{
+    //intializes cadence
     private MilitaryCadence cadence;
-    
+    //constructor which defines cadence with the argument
     public EverywhereCommand(MilitaryCadence cadence) {
         this.cadence = cadence;   
     }
-
+    //plays the song Everywhere We Go
     public void execute() {
         cadence.singEverywhereWeGo();
     }
--- InArmyCommand.java
+++ InArmyCommand.java
@@ -1,10 +1,15 @@
+/**
+ * A class to play specific song InArmy
+ * @author adam sanfacon
+ */
 public class InArmyCommand implements Command {
+    //initializes cadence variable
     private MilitaryCadence cadence;
-
+    //constructor for class to define cadence
     public InArmyCommand(MilitaryCadence cadence) {
         this.cadence = cadence;
     }
-
+    //executes song 
     public void execute() {
         cadence.singInArmy();
     }
