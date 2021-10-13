b2eef99de60b97d67f6b1a1a1517f62e7adb89c8
Veugenio01 <77406323+Veugenio01@users.noreply.github.com>
2021-10-13 13:00:02 -0400

    working input handler (kindof), no javaDoc

--- InputHandler.java
+++ InputHandler.java
@@ -1,14 +1,25 @@
 import java.util.ArrayList;
 
 public class InputHandler {
-    private ArrayList<Command> commands;
+    private Command[] commands;
+    // private static final String EVERYWHERE = "command/everywhere.txt";
+    // private static final String IDONTKNOW = "command/idontknow.txt";
+    // private static final String INARMY = "command/inarmy.txt";
 
     public InputHandler(MilitaryCadence cadence) {
+        IDontKnowCommand idk = new IDontKnowCommand(cadence);
+        InArmyCommand inArmy = new InArmyCommand(cadence);
+        EverywhereCommand everywhere = new EverywhereCommand(cadence);
 
+        commands = new Command[] { idk, inArmy, everywhere };
     }
 
     public boolean playCadence(int num) {
-        return true;
+        if (num < commands.length && num >= 0) {
+            commands[num].execute();
+            return true;
+        }
+        return false;
     }
 
 }
