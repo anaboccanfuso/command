dcdf6db08bf14aeb1bec24ee574ccbf2b152f91c
Veugenio01 <77406323+Veugenio01@users.noreply.github.com>
2021-10-13 15:34:46 -0400

    now has javaDoc

--- InputHandler.java
+++ InputHandler.java
@@ -1,8 +1,18 @@
+
+/**
+ * Handles the input from the user
+ * @author Vaughn Eugenio
+ */
 import java.util.ArrayList;
 
 public class InputHandler {
     private ArrayList<Command> commands;
 
+    /**
+     * constructor, created an arraylist of commands based on the passed in cadence
+     * 
+     * @param cadence - the type that is the song
+     */
     public InputHandler(MilitaryCadence cadence) {
         IDontKnowCommand idk = new IDontKnowCommand(cadence);
         InArmyCommand inArmy = new InArmyCommand(cadence);
@@ -14,6 +24,12 @@ public class InputHandler {
         commands.add(everywhere);
     }
 
+    /**
+     * exicutes the command for the desired cadence
+     * 
+     * @param num - index of the users desired cadence
+     * @return True if guess is valid, flase if otherwise
+     */
     public boolean playCadence(int num) {
         if (num < commands.size() && num >= 0) {
             commands.get(num).execute();
